f5170c3adcc04877b1f162ba3e5c2ff7f1b9afc0
--global <--global>
2025-05-16 11:03:40 +0500

    js 3-dars if else

---
+++ index.js
@@ -0,0 +1,97 @@
+//if(shart){
+//shart tog'ri bo'lsa bajariladigan code yoki amal
+//}else{
+//shart not'gri bo'lsa bajariladigan kod yoki amal
+//}
+
+// let yosh = 6;
+
+// if (yosh >= 18) {
+//   console.log("ovoz berish mumkin");
+// } else {
+//   console.log(
+//     `ovoz berish uchun yoshlik qilasiz. ${
+//       18 - yosh
+//     } yildan so'ng ovoz berish huquqiga ega bo'lasiz`
+//   );
+// }
+
+//if(shart){
+//shart tog'ri bo'lsa bajariladigan code yoki amal
+//}else if(shart){
+//shart tog'ri bo'lsa bajariladigan code yoki amal
+//}else if(shart){
+//shart tog'ri bo'lsa bajariladigan code yoki amal
+//}else if(shart){
+//shart tog'ri bo'lsa bajariladigan code yoki amal
+//}else{
+//yuqoridagi shartlarning barchasi notog'ri bo'lsa bajariladigan ish
+//}
+
+// ob_havo = ""; //qorli, quyoshli, shamolli, bulutli,
+// if (ob_havo == "yomg'irli") {
+//   console.log(`havo yomg'irli bo'ladi. soyabon olishni unutmang`);
+// } else if (ob_havo == "qorli") {
+//   console.log(`ob-havo qorli bo'ladi issiqroq kiyinib oling`);
+// } else if (ob_havo == "quyoshli") {
+//   console.log(
+//     `ob-havo quyoshli bo'ladi shlyapa kiyib oling sizni oftob urushi mumkin`
+//   );
+// } else if (ob_havo == "shamolli") {
+//   console.log(
+//     "ob-havo shamolli bo'ladi shamol soyaboningizni uchirib yuborishi mumkin."
+//   );
+// } else if (ob_havo == "bulutli") {
+//   console.log("ob-havo bulutli bo'ladi salqin havoda sayrga chiqing");
+// } else {
+//   console.log("kechirasiz ob-havo ma'lumoti mavjud emas");
+//  }
+//
+
+// let son = 22;
+
+// if (son % 2 == 1) console.log("toq son");
+// else console.log("juft son");
+
+// shart ? shart tog'ri bo'lsa bajariladigan kod : shart notog'ri bo'lsa bajariladigan kod
+
+// son % 2 == 1 ? console.log("toq son") : console.log("juft son");
+
+// let title = "";
+
+// if (title == "Webbrain academy") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// // }
+
+// if ((title = 0)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// console.log((title = false));
+
+// if (title) console.log(true);
+// else console.log(false);
+
+// ob_havo = "qorli"; //qorli, quyoshli, shamolli, bulutli,
+// ob_havo == "yomg'irli"
+//   ? console.log(`havo yomg'irli bo'ladi. soyabon olishni unutmang`)
+//   : ob_havo == "qorli"
+//   ? console.log(`ob-havo qorli bo'ladi issiqroq kiyinib oling`)
+//   : ob_havo == "quyoshli"
+//   ? console.log(
+//       `ob-havo quyoshli bo'ladi shlyapa kiyib oling sizni oftob urushi mumkin`
+//     )
+//   : ob_havo == "shamolli"
+//   ? console.log(
+//       "ob-havo shamolli bo'ladi shamol soyaboningizni uchirib yuborishi mumkin."
+//     )
+//   : ob_havo == "bulutli"
+//   ? console.log("ob-havo bulutli bo'ladi salqin havoda sayrga chiqing")
+//   : console.log("kechirasiz ob-havo ma'lumoti mavjud emas");
+
+// let title = 8;
+// title == "webbrain academy" ? console.log(true) : console.log(false);
---
+++ node.md
@@ -0,0 +1,30 @@
+if - agar
+else - aks holda
+if(shart){
+shart tog'ri bo'lsa bajariladigan ish
+}else{
+shart notog'ri bo'lsa bajariladigan ish
+}
+
+@topshiriq yosh nomli o'zgaruvchi yarating va agar yosh 18dan katta yoki teng bo'lsa terminalda ovozberish mumkin aks holda ovoz berish mumkin emas yozuvhi chiqsin
+
+agar 1tadan ko'proq shartlarni tekshirishimiz kk bo'lsa else ifdan foydalanamiz
+if(shart){
+shart tog'ri bo'lsa bajariladigan ish
+}else if(shart){
+shart tog'ri bo'lsa bajariladigan ish
+}else if(shart){
+shart tog'ri bo'lsa bajariladigan ish
+}else{
+shart notog'ri bo'lsa bajariladigan ish
+}
+
+@topshiriq else ifdan foydalanib ob-havo haqida ma'lumot beradigan code yozing
+
+ternary operatori
+shart ? shart tog'ri bo'lsa bajariladigan ish :shart notog'ri bo'lsa bajariladigan ish
+agar biz faqat 2ta holatni tekshiradigan bo'lsak ternary operatoridan foydalangamiz yaxshiroq.
+
+@topshiriq JS homeworkdagi vazifalarni ishlab bering
+
+@topshiriq 
